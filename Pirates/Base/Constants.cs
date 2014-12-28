511c05148f00f2025374591e74e9bcfee5716c76
Tjesto <szycha6@gmail.com>
2014-12-28 01:15:12 +0100

    Collision detection added

---
+++ Pirates/Base/Constants.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    public enum FieldType 
+    { 
+        WATER = 0,
+        LAND = 1,
+        CITY = 2        
+    }
+
+    public class MapFieldsType
+    {
+        public FieldType type { set; get; }
+        public string name { set; get; }
+
+        public MapFieldsType(int type, string name) : this((FieldType) type, name)
+        {
+            //empty;
+        }
+
+        public MapFieldsType(FieldType type, string name)
+        {
+            this.type = type;
+            this.name = name;
+        }
+        
+    }
+}
